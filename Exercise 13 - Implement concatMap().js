7bd44a66e6cef6c821a1a2aa01fda651d0564706
David Ernst <git@dsernst.com>
2015-09-02 09:50:40 -0700

    Properly add concatAll to concatMap

--- Exercise 13 - Implement concatMap().js
+++ Exercise 13 - Implement concatMap().js
@@ -1,3 +1,5 @@
+Array.prototype.concatAll = require('./concatAll');
+
 // Exercise 13: Implement concatMap()
 
 // Nearly every time we flatten a tree we chain map() and concatAll(). Sometimes, if we're dealing with a tree several levels deep, we'll repeat this combination many times in our code. To save on typing, let's create a concatMap function that's just a map operation, followed by a concatAll.
